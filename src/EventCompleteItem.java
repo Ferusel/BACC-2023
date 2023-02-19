32ead1b8eaaaa25ee29554d64a14004a191d9956
Ferusel <wheatgrass.mc@gmail.com>
2023-02-19 15:43:32 +0800

    Add output file for completed Items

--- src/EventCompleteItem.java
+++ src/EventCompleteItem.java
@@ -9,6 +9,10 @@ class EventCompleteItem extends Event {
         this.i = i;
     }
 
+    public Item getItem() {
+        return this.i;
+    }
+
     @Override
     public String toString() {
         return super.toString() + String.format(": EVENT_COMPLETE_ITEM : One Item completed (%s)", this.i);
